fdf6da33f6b71194f3b4bcdd7fd2f3ded40ce0b1
FranciscaCMG <francisca.cmg@gmail.com>
2023-10-13 20:08:16 -0300

    nuevo proyecto

--- ev1/src/main/java/cl/usach/tingeso/ev1/repositories/EstudianteRepository.java
+++
@@ -1,9 +0,0 @@
-package cl.usach.tingeso.ev1.repositories;
-
-import cl.usach.tingeso.ev1.entities.EstudianteEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstudianteRepository extends JpaRepository<EstudianteEntity, String> {
-}
